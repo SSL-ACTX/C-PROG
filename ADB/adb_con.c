d66794cb881982fd27723f53e21a96863cba506c
LLDB <120081647+SSL-ACTX@users.noreply.github.com>
2022-12-09 11:00:00 +0800

    Add files via upload
    Unfinished..

---
+++ ADB/adb_con.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define p printf
+#define s scanf
+#define st system
+
+    char ex;
+
+int main() {
+
+    p("\nChoose a Function: ");
+    p("\nA. ADB Connect    B. ADB Disconnect");
+    p("\nC. Full Dump      D. Battery Status");
+    p("\nE. Watch Logcat   F. Devices");
+    p("\nG. Private Inf.   H. Reboot (Advance)");
+    p("\nI. Help           J. Bug Report");
+    p("\nX. Restart ADB as ROOT");
+    p("\n");
+    s("%c", &ex);
+
+
+    switch(ex) {
+
+        case 'a': case 'A':
+          p("\nThis will take some time, please wait...");
+          st("adb connect 192.168.0.100");
+          st("adb connect 192.168.0.101");
+          st("adb connect 192.168.0.102");
+          st("adb connect 192.168.0.103");
+          st("adb connect 192.168.0.104");
+          st("adb connect 192.168.0.105");
+          st("adb connect 192.168.0.106");
+          st("adb connect 192.168.0.107");
+          st("cls");
+          p("Connected");
+          break;
+
+        case 'b': case 'B':
+          p("\nDisconnecting...");
+          st("adb disconnect");
+          st("adb kill-server");
+          break;
+
+        case 'c': case 'C':
+          p("\nDumping...");
+          st("adb shell dumpsys");
+          break;
+
+        case 'd': case 'D':
+          p("\nBattery Status");
+          st("adb shell dumpsys battery");
+          break;
+
+        case 'e': case 'E':
+          p("\nLogcat");
+          st("adb logcat");
+          break;
+
+        case 'f': case 'F':
+          p("\nDevices");
+          st("adb devices");
+          break;
+
+        case 'g': case 'G'://dumpsys.exe
+          st("X:/c_proj/adb/dumpsys.exe");
+          break;
+
+        case 'h': case 'H'://reboot.exe
+          st("X:/c_proj/adb/reboot.exe");
+          break;
+
+        case 'i': case 'I':
+          p("\nHelp");
+          st("adb help");
+          break;
+
+        case 'j': case 'J':
+          p("adb bugreport");
+          break;
+
+        case 'x': case 'X':
+          p("\nStarting as ROOT...");
+          st("adb root");
+          break;
+    }
+  return 0;
+}
---
+++ ADB/dumpsys.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#define p printf
+#define s scanf
+#define st system
+
+   char ch;
+
+int main() {
+
+    p("\nOther Features");
+    p("\n1. Device State    2. Serial No.");
+    p("\n3. IMEI            4. Netstat");
+    p("\n5. Phone Features  6. Services");
+    p("\n7. Process Status  8. Screen Resolution");
+    p("\n9. Backup ALL      10.List Packages (10-10.5)");
+    p("\n");
+    s("%c", &ch);
+
+    switch(ch) {
+
+        case '1':
+        st("adb get-state");
+        break;
+
+        case '2':
+        st("adb get-serialno");
+        break;
+
+        case '3':
+        st("adb shell dumpsys iphonesybinfo");
+        break;
+
+        case '4':
+        st("adb shell netstat");
+        break;
+
+        case '5':
+        st("adb shell pm list features");
+        break;
+
+        case '6':
+        st("adb shell service list");
+        break;
+
+        case '7':
+        st("adb shell ps");
+        break;
+
+        case '8':
+        st("adb shell wm size");
+        break;
+
+        case '9':
+        st("adb backup");
+        break;
+
+        case '10':
+        st("adb list packages");
+        break;
+
+        case '10.1':
+        st("adb list packages -r");
+        break;
+
+        case '10.2':
+        st("adb list packages -3");
+        break;
+
+        case '10.3':
+        st("adb list packages -s");
+        break;
+
+        case '10.4':
+        st("adb list packages -u");
+        break;
+
+        case '10.5':
+        st("adb shell dumpsys package packages");
+        break;
+    }
+    return 0;
+}
